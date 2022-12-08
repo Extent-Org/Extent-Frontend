40318d4a7d7fa522d923209cef31e52c19454208
Darshan Hande <darshanhande11@gmail.com>
2022-12-08 12:47:20 +0530

    added sass files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from './components/LandingPage/LandingPage';
+import PageLayout from './components/PageLayout/PageLayout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
