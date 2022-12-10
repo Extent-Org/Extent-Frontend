2a6f44294a05632cb1b7a026c5e519b39af7cccd
Darshan Hande <darshanhande11@gmail.com>
2022-12-11 00:54:45 +0530

    :sparkles: added animation to grab nft comp

--- src/components/LandingPage/GrabNFT/GrabNFT.tsx
+++ src/components/LandingPage/GrabNFT/GrabNFT.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './GrabNFT.scss'
 import { RxCornerBottomLeft, RxCornerBottomRight } from 'react-icons/rx'
+import { motion } from 'framer-motion'
 
 const GrabNFT = () => {
   return (
@@ -14,16 +15,58 @@ const GrabNFT = () => {
                     <img src="/assets/images/grab-nft-vertical-card.png" alt="grab-nft-vertical" className='gnft-card-img-1' />
                     <img src="/assets/images/grab-nft-vertical-card.png" alt="grab-nft-vertical" className='gnft-card-img-2' />
                 </div> */}
-                <div className='gnft-img-stack'>
+                <motion.div className='gnft-img-stack'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    transition={{ 
+                        duration: 0.3,
+                        default: {
+                            duration: 0.3,
+                            ease: [0, 0.71, 0.2, 1.01]
+                          },
+                          scale: {
+                            type: "spring",
+                            damping: 5,
+                            stiffness: 100,
+                            restDelta: 0.001
+                          }
+                    }}
+                    variants={{
+                      visible: { opacity: 1, scale: 1 },
+                      hidden: { opacity: 0, scale: 0 }
+                    }}
+                >
                     <img src="/assets/images/grab-nft-creator.png" alt="creator-card" className='gnft-creator-img' />
-                </div>
-                <div className='gnft-token-div-0'>
+                </motion.div>
+                <motion.div className='gnft-token-div-0'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    transition={{ 
+                        duration: 0.3,
+                        default: {
+                            duration: 0.3,
+                            ease: [0, 0.71, 0.2, 1.01]
+                          },
+                          scale: {
+                            type: "spring",
+                            damping: 5,
+                            stiffness: 100,
+                            restDelta: 0.001
+                          }
+                    }}
+                    variants={{
+                      visible: { opacity: 1, scale: 1 },
+                      hidden: { opacity: 0, scale: 0 }
+                    }}
+                >
                     <div className='gnft-token-div-1'>
                         <div className='gnft-token-div-2'>
                             <img src='/assets/images/grab-nft-token.png' alt='nft-token' className='gnft-token-img' />
                         </div>
                     </div>
-                </div>
+                </motion.div>
             </div>
         </div>
         <div className='gnft-braces-div'>
