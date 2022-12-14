93c4c7393e42816beeac9347c1b89e6aba00896e
Darshan Hande <darshanhande11@gmail.com>
2022-12-14 17:14:19 +0530

    :white_check_mark: added validation for Input field.

--- src/components/LandingPage/Newsletter/Newsletter.tsx
+++ src/components/LandingPage/Newsletter/Newsletter.tsx
@@ -8,8 +8,15 @@ const Newsletter = () => {
   const emailInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   }
+  const [isError, setIsError] = useState(false);
   const emailSubmitHandler = (e: React.FormEvent) => {
     e.preventDefault();
+    if(email === '' || !email.includes('@') || !email.includes('.')) {
+      setIsError(true);
+      alert('Please enter a valid email address!');
+      return;
+    }
+    setIsError(false);
     alert(`Wohoo! You have subscribed to our newsletter with ${email}`);
     setEmail('');
   }
@@ -24,7 +31,7 @@ const Newsletter = () => {
           className='nl-form'
         >
           <InputGroup className='nl-input-div'>
-            <Input placeholder='Your email' value={email} onChange={emailInputHandler} size='lg' variant='filled' className='nl-input' />
+            <Input placeholder='Your email' isInvalid={isError} value={email} onChange={emailInputHandler} size='lg' variant='filled' errorBorderColor='crimson' className='nl-input' />
             <InputRightElement className='nl-input-btn' children={<BiRightArrowAlt type='submit' onClick={emailSubmitHandler} />} />
           </InputGroup>
         </form>
