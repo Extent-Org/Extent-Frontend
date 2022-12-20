4313bbbe208f5b697234f64a03d607a7724e970b
Himanshu Kumar Verma <76449638+Himanshuiiit@users.noreply.github.com>
2022-12-20 17:51:00 +0530

    add stream rate to music page

--- src/components/UploadPage/UploadePages/UploadMusic.tsx
+++ src/components/UploadPage/UploadePages/UploadMusic.tsx
@@ -3,12 +3,14 @@ import MusicInput from '../UploadPageComponents/MusicInput'
 import PictureInput from '../UploadPageComponents/PictureInput'
 import PublishButton from '../UploadPageComponents/PublishButton'
 import TitleAndDescInput from '../UploadPageComponents/TitleAndDescInput'
+import StreamRateInput from "../UploadPageComponents/StreamRateInput";
 
 const UploadMusic = () => {
   return (
     <div>
       <TitleAndDescInput ofType="music" />
       <PictureInput ofType="Cover Photo" size={1} />
+      <StreamRateInput />
       <MusicInput />
       <PublishButton />
     </div>
