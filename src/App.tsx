9240ce7e6cfd39f965cadfab8e383f094ace83ce
Nitanshu Lokhande <56120084+nlok5923@users.noreply.github.com>
2022-12-20 21:58:50 +0530

    Revert "➕ add routes and content page for creator"

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
-import React from "react";
-import "./App.scss";
-import "antd/dist/reset.css";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import LandingPage from "./components/LandingPage/LandingPage";
-import CreatorDashbaord from "./components/CreatorPage/CreatorDashboard/CreatorDashbaord";
-import CreatorContent from "./components/CreatorPage/CreatorContent/CreatorContent";
-import Creator from "./components/CreatorPage/Creator";
-import CreatorDraft from "./components/CreatorPage/CreatorDraft/CreatorDraft";
-import CreatorAnalytics from "./components/CreatorPage/CreatorAnalytics/CreatorAnalytics";
-import CreatorProfile from "./components/CreatorPage/CreatorProfile/CreatorProfile";
+import React from 'react';
+import './App.scss';
+import 'antd/dist/reset.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/creator" element={<Creator />}>
-            <Route path="dashboard" element={<CreatorDashbaord />} />
-            <Route path="content" element={<CreatorContent />} />
-            <Route path="draft" element={<CreatorDraft />} />
-            <Route path="analytics" element={<CreatorAnalytics />} />
-            <Route path="profile" element={<CreatorProfile />} />
-            <Route path="" element={<Navigate to="dashboard" replace />} />
-          </Route>
+          <Route path="/" element={<LandingPage/>} />
         </Routes>
       </Router>
     </div>
--- src/components/CreatorPage/CreatorDraft/CreatorDraft.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CreatorDraft = () => {
-  return (
-    <div>CreatorDraft</div>
-  )
-}
-
-export default CreatorDraft
