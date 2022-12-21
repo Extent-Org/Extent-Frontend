2dd8401ad64d733bc7fecbf31491cbdbc78570de
nlok5923 <nlokhande5923@gmail.com>
2022-12-21 22:55:29 +0530

    integrating rainbow kit with wagmi.sh

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./App.scss";
 import "antd/dist/reset.css";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import CreatorDashbaord from "./components/CreatorPage/CreatorDashboard/CreatorDashbaord";
 import CreatorContent from "./components/CreatorPage/CreatorContent/CreatorContent";
@@ -9,23 +14,44 @@ import Creator from "./components/CreatorPage/Creator";
 import CreatorDraft from "./components/CreatorPage/CreatorDraft/CreatorDraft";
 import CreatorAnalytics from "./components/CreatorPage/CreatorAnalytics/CreatorAnalytics";
 import CreatorProfile from "./components/CreatorPage/CreatorProfile/CreatorProfile";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { goerli, configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains([goerli], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "Extent App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/creator" element={<Creator />}>
-            <Route path="dashboard" element={<CreatorDashbaord />} />
-            <Route path="content" element={<CreatorContent />} />
-            <Route path="draft" element={<CreatorDraft />} />
-            <Route path="analytics" element={<CreatorAnalytics />} />
-            <Route path="profile" element={<CreatorProfile />} />
-            <Route path="" element={<Navigate to="dashboard" replace />} />
-          </Route>
-        </Routes>
-      </Router>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/creator" element={<Creator />}>
+                <Route path="dashboard" element={<CreatorDashbaord />} />
+                <Route path="content" element={<CreatorContent />} />
+                <Route path="draft" element={<CreatorDraft />} />
+                <Route path="analytics" element={<CreatorAnalytics />} />
+                <Route path="profile" element={<CreatorProfile />} />
+                <Route path="" element={<Navigate to="dashboard" replace />} />
+              </Route>
+            </Routes>
+          </Router>
+        </RainbowKitProvider>
+      </WagmiConfig>
     </div>
   );
 }
