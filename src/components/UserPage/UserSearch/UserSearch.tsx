f065d0bac4a471ebca85bb340b141645ae818693
Vijay Karanjkar <vijaykaranjkar2003@gmail.com>
2023-01-01 12:56:56 +0530

    Feat: Create Search Bar

--- src/components/UserPage/UserSearch/UserSearch.tsx
+++ src/components/UserPage/UserSearch/UserSearch.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { AiOutlineSearch } from 'react-icons/ai';
+import "./UserSearch.scss";
 
 const UserSearch = () => {
   return (
-    <div>
-      user search
+    <div className="search">
+        <div className="search__wrapper">
+          <div className="search__wrapper-bar">
+            <div className="search__wrapper-bar_icon"><AiOutlineSearch /></div>
+            <div className="search__wrapper-bar_input">
+              <input type="text" name="search" id="search_input" placeholder='Search' />
+            </div>
+          </div>
+          <div className="search__wrapper-trending">
+            <div className="search__wrapper-trending_title">
+              <span>Trending</span>
+            </div>
+            <div className="search__wrapper-trending_cards">
+              Cards
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
