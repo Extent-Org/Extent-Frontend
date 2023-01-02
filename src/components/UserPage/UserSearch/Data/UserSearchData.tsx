a296b93ff8e2a203f85255aa816961433bc7f654
Vijay Karanjkar <vijaykaranjkar2003@gmail.com>
2023-01-02 18:27:44 +0530

    Feat: Search Suggestions

---
+++ src/components/UserPage/UserSearch/Data/UserSearchData.tsx
@@ -0,0 +1,60 @@
+export type UserSearchData = {
+    imgpath: string;
+    name: string;
+    username: string;
+};
+
+const SearchData: UserSearchData[] = [
+    {
+        imgpath: "/assets/images/photos/profilePic_lg.png",
+        name: "Nobuhara",
+        username: "noburockshere"
+    },
+    {
+        imgpath: "/assets/images/photos/profilePic_lg.png",
+        name: "Diego Alves",
+        username: "codeAlves"
+    },
+    {
+        imgpath: "/assets/images/photos/profilePic_lg.png",
+        name: "Emi Martinez",
+        username: "martinezgoalie"
+    },
+    {
+        imgpath: "/assets/images/photos/profilePic_lg.png",
+        name: "Paulo Dybala",
+        username: "dybalaJuve"
+    },
+    {
+        imgpath: "/assets/images/photos/profilePic_lg.png",
+        name: "Max Aflfredo",
+        username: "africa101"
+    },
+    {
+        imgpath: "/assets/images/photos/profilePic_lg.png",
+        name: "Nobuhara",
+        username: "noburockshere"
+    },
+    {
+        imgpath: "/assets/images/photos/profilePic_lg.png",
+        name: "Diego Alves",
+        username: "codeAlves"
+    },
+    {
+        imgpath: "/assets/images/photos/profilePic_lg.png",
+        name: "Emi Martinez",
+        username: "martinezgoalie"
+    },
+    {
+        imgpath: "/assets/images/photos/profilePic_lg.png",
+        name: "Paulo Dybala",
+        username: "dybalaJuve"
+    },
+    {
+        imgpath: "/assets/images/photos/profilePic_lg.png",
+        name: "Max Aflfredo",
+        username: "africa101"
+    },
+];
+
+export default SearchData;
--- src/components/UserPage/UserSearch/UserSearch.tsx
+++ src/components/UserPage/UserSearch/UserSearch.tsx
@@ -1,37 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Card from "../UserSharedComponents/Card/Card";
 import { AiOutlineSearch } from 'react-icons/ai';
 import "./UserSearch.scss";
+import UserData, { UserSearchData } from "./Data/UserSearchData";
 
 const UserSearch = () => {
+  const [search, setSearch] = useState("");
+  const arr: UserSearchData[] = [];
+  const [searchrecord, setSearchRecord] = useState(arr);
+  console.log(searchrecord);
+  useEffect(() => {
+    if (search === "") {
+      setSearchRecord(arr);
+      return;
+    }
+    const data = UserData.filter((creator) => creator.name.toLowerCase().includes(search.toLowerCase()) || creator.username.toLowerCase().includes(search.toLowerCase()));
+    setSearchRecord(data);
+  }, [search]);
+
   return (
     <div className="search">
-        <div className="search__wrapper">
-          <div className="search__wrapper-bar">
-            <div className="search__wrapper-bar_icon"><AiOutlineSearch /></div>
-            <div className="search__wrapper-bar_input">
-              <input type="text" name="search" id="search_input" placeholder='Search' />
-            </div>
-          </div>
-          <div className="search__wrapper-trending">
-            <div className="search__wrapper-trending_title">
-              <span>Trending Creators</span><img src="/assets/images/icons/qrcode.svg" alt="QR Code" />
-            </div>
-            <div className="search__wrapper-trending_cards">
-              <Card imgpath="/assets/images/photos/profilePic_lg.png" name="Nobuhara" username="noburockshere" />
-              <Card imgpath="/assets/images/photos/profilePic_lg.png" name="Nobuhara" username="noburockshere" />
-              <Card imgpath="/assets/images/photos/profilePic_lg.png" name="Nobuhara" username="noburockshere" />
-              <Card imgpath="/assets/images/photos/profilePic_lg.png" name="Nobuhara" username="noburockshere" />
-              <Card imgpath="/assets/images/photos/profilePic_lg.png" name="Nobuhara" username="noburockshere" />
-              <Card imgpath="/assets/images/photos/profilePic_lg.png" name="Nobuhara" username="noburockshere" />
-              <Card imgpath="/assets/images/photos/profilePic_lg.png" name="Nobuhara" username="noburockshere" />
-              <Card imgpath="/assets/images/photos/profilePic_lg.png" name="Nobuhara" username="noburockshere" />
-
-            </div>
+      <div className="search__wrapper">
+        <div className="search__wrapper-bar">
+          <div className="search__wrapper-bar_icon"><AiOutlineSearch /></div>
+          <div className="search__wrapper-bar_input">
+            <input
+              type="text"
+              name="search"
+              id="search_input"
+              placeholder='Search'
+              onChange={(e) => setSearch(e.target.value)}
+            />
           </div>
         </div>
+        {
+          searchrecord.length != 0 ?
+            <div className="search__wrapper-trending">
+              <div className="search__wrapper-trending_cards">
+                {
+                  searchrecord.map((creator, idx) => {
+                    return <Card key={idx} imgpath={creator.imgpath} name={creator.name} username={creator.username} />;
+                  })
+                }
+              </div>
+            </div>
+            : null
+        }
+        <div className="search__wrapper-trending">
+          <div className="search__wrapper-trending_title">
+            <span>Trending Creators</span><img src="/assets/images/icons/qrcode.svg" alt="QR Code" />
+          </div>
+          <div className="search__wrapper-trending_cards">
+            {
+              UserData.map((creator, idx) => {
+                return <Card key={idx} imgpath={creator.imgpath} name={creator.name} username={creator.username} />;
+              })
+            }
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserSearch
+export default UserSearch;
