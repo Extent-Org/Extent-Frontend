a4bac955cb6588a034dad4a9af403c203a3f9884
nlok5923 <nlokhande5923@gmail.com>
2023-01-04 22:05:57 +0530

    removed unneeded errors

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+const { ProvidePlugin }= require("webpack")
+
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    buffer: require.resolve("buffer"),
+    crypto: require.resolve("crypto-browserify"),
+  }
+  config.resolve.extensions = [...config.resolve.extensions, ".ts", ".js"]
+  config.plugins = [
+    ...config.plugins,
+    new ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ]
+  console.log(config.resolve)
+  console.log(config.plugins)
+
+  return config
+}
